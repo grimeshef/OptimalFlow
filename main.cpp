7513b41e632bd199cf7e142e4f30046b58e9fc78
grimeshef <alexmaltsevvik@gmail.com>
2023-01-10 14:45:21 +0300

    blink led

--- main.cpp
+++ main.cpp
@@ -5,8 +5,22 @@
 
 #include "mbed.h"
 
+Thread blink;
+
+
+[[noreturn]] void blink_loop(){
+    DigitalOut led1(PF_13, 1);
+    while (true) {
+        auto start_time = Kernel::Clock::now().time_since_epoch();
+        led1 = !led1;
+        auto delta_time = start_time - Kernel::Clock::now().time_since_epoch();;
+        ThisThread::sleep_for(chrono::milliseconds(500ms - delta_time));
+    }
+}
+
 
 int main()
 {
+    blink.start(callback(blink_loop));
     return 0;
 }
